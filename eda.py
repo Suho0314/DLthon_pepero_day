239ade429e7901807b8510fb1bb2734a168f1dbc
Suho0314 <suholee0314@gmail.com>
2025-11-11 14:11:17 +0900

    [LSH] Last commit

--- eda.py
+++ eda.py
@@ -2,7 +2,7 @@ from dataset import create_dataloaders
 from utils import hist_conversations_length
 
 # 파일 경로 설정
-train_file_path = "./Data/aiffel-dl-thon-dktc-online-15/train_agg.csv"
+train_file_path = "./Data/aiffel-dl-thon-dktc-online-15/train.csv"
 test_file_path = "./Data/aiffel-dl-thon-dktc-online-15/test.csv"
 
 # max_length 확인하기
